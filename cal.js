8375fab20579e205a271a8be1fe0a8550162beed
Aamir <aamirhafiez@gmail.com>
2020-07-15 11:59:15 +0530

    Fixed issue with infinity

--- cal.js
+++ cal.js
@@ -7,26 +7,30 @@ var button = document.querySelectorAll('.button');
 for(var i = 0; i < button.length; i++){
 	button[i].addEventListener('click', function(){
 		var but = this.getAttribute('data-value');
+		var isInfinity = false;
+		if(calText.textContent.toLowerCase() === 'infinity'){
+			isInfinity = true;
+		}
 		if(but === 'AC'){
 			op1 = 0;
 			op2 = null;
 			operator = null;
 			calText.innerText = "0";
 		}
-		else if(but === '+/-'){
+		else if(but === '+/-' && !isInfinity){
 			var dis = parseFloat(calText.textContent);
 			dis = - dis;
 			calText.innerText = dis;
 		}
-		else if (but == '%') {
+		else if (but == '%' && !isInfinity) {
 			var dis = calText.innerText;
 			calText.innerText = dis + '%';
 		}
-		else if (but == '=') {
+		else if (but == '=' && !isInfinity) {
 			var dis = calText.innerText;
 			calText.innerText = eval(dis);
 		}
-		else if (but == '1') {
+		else if (but == '1' && !isInfinity) {
 			var dis = calText.textContent;
 			if(dis === '0'){
 				calText.innerText = '1';
@@ -34,7 +38,7 @@ for(var i = 0; i < button.length; i++){
 				calText.innerText = dis + '1'; 
 			}
 		}
-		else if (but == '2') {
+		else if (but == '2' && !isInfinity) {
 			var dis = calText.textContent;
 			if(dis === '0'){
 				calText.innerText = '2';
@@ -42,7 +46,7 @@ for(var i = 0; i < button.length; i++){
 				calText.innerText = dis + '2'; 
 			}
 		}
-		else if (but == '3') {
+		else if (but == '3' && !isInfinity) {
 			var dis = calText.textContent;
 			if(dis === '0'){
 				calText.innerText = '3';
@@ -50,7 +54,7 @@ for(var i = 0; i < button.length; i++){
 				calText.innerText = dis + '3'; 
 			}
 		}
-		else if (but == '4') {
+		else if (but == '4' && !isInfinity) {
 			var dis = calText.textContent;
 			if(dis === '0'){
 				calText.innerText = '4';
@@ -58,7 +62,7 @@ for(var i = 0; i < button.length; i++){
 				calText.innerText = dis + '4'; 
 			}
 		}
-		else if (but == '5') {
+		else if (but == '5' && !isInfinity) {
 			var dis = calText.textContent;
 			if(dis === '0'){
 				calText.innerText = '5';
@@ -66,7 +70,7 @@ for(var i = 0; i < button.length; i++){
 				calText.innerText = dis + '5'; 
 			}
 		}
-		else if (but == '6') {
+		else if (but == '6' && !isInfinity) {
 			var dis = calText.textContent;
 			if(dis === '0'){
 				calText.innerText = '6';
@@ -74,7 +78,7 @@ for(var i = 0; i < button.length; i++){
 				calText.innerText = dis + '6'; 
 			}
 		}
-		else if (but == '7') {
+		else if (but == '7' && !isInfinity) {
 			var dis = calText.textContent;
 			if(dis === '0'){
 				calText.innerText = '7';
@@ -82,7 +86,7 @@ for(var i = 0; i < button.length; i++){
 				calText.innerText = dis + '7'; 
 			}
 		}
-		else if (but == '8') {
+		else if (but == '8' && !isInfinity) {
 			var dis = calText.textContent;
 			if(dis === '0'){
 				calText.innerText = '8';
@@ -90,7 +94,7 @@ for(var i = 0; i < button.length; i++){
 				calText.innerText = dis + '8'; 
 			}
 		}
-		else if (but == '9') {
+		else if (but == '9' && !isInfinity) {
 			var dis = calText.textContent;
 			if(dis === '0'){
 				calText.innerText = '9';
@@ -98,31 +102,29 @@ for(var i = 0; i < button.length; i++){
 				calText.innerText = dis + '9'; 
 			}
 		}
-		else if (but == '0') {
+		else if (but == '0' && !isInfinity) {
 			var dis = calText.textContent;
 			if(dis !== '0'){
 				calText.innerText = dis + '0';
 			}
 		}
-		else if (but == '/') {
+		else if (but == '/' && !isInfinity) {
 			var dis = calText.textContent;
-			if(dis !== '0'){
-				calText.innerText = dis + '/'; 
-			}
+			calText.innerText = dis + '/'; 
 		}
-		else if (but == '*') {
+		else if (but == '*' && !isInfinity) {
 			var dis = calText.innerText;
 			calText.innerText = dis + '*';
 		}
-		else if (but == '+') {
+		else if (but == '+' && !isInfinity) {
 			var dis = calText.innerText;
 			calText.innerText = dis + '+';
 		}
-		else if (but == '-') {
+		else if (but == '-' && !isInfinity) {
 			var dis = calText.innerText;
 			calText.innerText = dis + '-';
 		}
-		else if (but == '.') {
+		else if (but == '.' && !isInfinity) {
 			var dis = calText.textContent;
 			if(dis.toString().indexOf('.') == -1){
 				calText.innerText = dis + '.';
